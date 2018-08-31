e2c4df574b70c4adb0f1bbe83fb0cefe82a40928
lmxia <lmxia@alauda.io>
2018-09-01 00:10:08 +0800

    ss

--- controls/image.py
+++ controls/image.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import os, time, inspect, skimage, logging
+import os, time, skimage, logging
 import numpy as np
 from utils.response import JsonResponse
 from utils.imagenet import *
