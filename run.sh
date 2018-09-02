fc79297bf9f97a4cd8816b7363921a897425c951
lmxia <lmxia@alauda.io>
2018-09-02 14:06:50 +0800

    add scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 curl -F 'file=@./tiger.jpeg' localhost:8000/image/process
+curl -X POST -H 'content-type:application/json' -d '{"points":[{"x":"1","y":"0.9"},{"x":"2.1","y":"1.9"},{"x":"2.9","y":"2.87"}]}' 18.220.97.191:8000/line/process
 git clone https://github.com/lmxia/education.git
 git clone https://github.com/tensorlayer/tensorlayer.git
 
