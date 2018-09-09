31b43a761e1240237270a282ec4d780f8e3508c8
lmxia <lmxia@alauda.io>
2018-09-09 21:40:27 +0800

    fix

--- controls/line.py
+++ controls/line.py
@@ -26,7 +26,7 @@ class LineControl(object):
     b = tf.Variable(tf.zeros([1]))
 
     # Construct a linear model，构造线性模型
-    pred = tf.matmul(X, W) + biases
+    pred = tf.matmul(X, W) + b
     # n_samples = 10
     # Mean squared error，损失函数：均方差
     cost = tf.reduce_mean(tf.square(pred - Y))
