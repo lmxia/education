14d00f07f0142ceea41ccf62f90bfe3a0d769da1
lmxia <lmxia@alauda.io>
2018-09-09 21:39:04 +0800

    fix

--- controls/line.py
+++ controls/line.py
@@ -17,18 +17,18 @@ class LineControl(object):
     y_list = []
     sess = tf.Session()
     # tf Graph Input，tf图输入
-    X = tf.placeholder("float")
-    Y = tf.placeholder("float")
+    num_input = 1
+    X = tf.placeholder(tf.float32, [None,num_input])
+    Y = tf.placeholder(tf.float32, [None,num_input])
 
     # Set model weights，初始化网络模型的权重
-    W = tf.Variable(rng.randn(), name="weight")
-    b = tf.Variable(rng.randn(), name="bias")
+    W = tf.Variable(tf.random_uniform([1,1], -1.0, 1.0))
+    b = tf.Variable(tf.zeros([1]))
 
     # Construct a linear model，构造线性模型
-    pred = tf.add(tf.multiply(X, W), b)
+    pred = tf.matmul(X, Weights) + biases
     # n_samples = 10
     # Mean squared error，损失函数：均方差
-    # cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
     cost = tf.reduce_mean(tf.square(pred - Y))
     # Gradient descent， 优化方式：梯度下降
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
@@ -45,21 +45,20 @@ class LineControl(object):
         
         train_X = numpy.array(cls.x_list)
         train_Y = numpy.array(cls.y_list)
-        # cls.n_samples = train_X.shape[0]
 
         # Fit all training data
         for epoch in range(cls.training_epochs):
-            for (x, y) in zip(train_X, train_Y):
-                cls.sess.run(cls.optimizer, feed_dict={cls.X: x, cls.Y: y})
+            # for (x, y) in zip(train_X, train_Y):
+            cls.sess.run(cls.optimizer, feed_dict={cls.X: train_X[:,np.newaxis], cls.Y: train_Y[:,np.newaxis]})
 
             # Display logs per epoch step
             if (epoch+1) % cls.display_step == 0:
-                c = cls.sess.run(cls.cost, feed_dict={cls.X: train_X, cls.Y:train_Y})
-                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),"W=", cls.sess.run(cls.W), "b=", cls.sess.run(cls.b))
+                c = cls.sess.run(cls.cost, feed_dict={cls.X: train_X[:,np.newaxis], cls.Y:train_X[:,np.newaxis]})
+                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),"W=", cls.sess.run(cls.W)[0][0], "b=", cls.sess.run(cls.b)[0])
                     
         print("Optimization Finished!")
-        training_cost = cls.sess.run(cls.cost, feed_dict={cls.X: train_X, cls.Y: train_Y})
-        print("Training cost=", training_cost, "W=", cls.sess.run(cls.W), "b=", cls.sess.run(cls.b), '\n')
-        back["w"] = cls.sess.run(cls.W)
-        back["b"] = cls.sess.run(cls.b)
+        training_cost = cls.sess.run(cls.cost, feed_dict={cls.X: train_X[:,np.newaxis], cls.Y: train_Y[:,np.newaxis]})
+        print("Training cost=", training_cost, "W=", cls.sess.run(cls.W)[0][0], "b=", cls.sess.run(cls.b)[0], '\n')
+        back["w"] = cls.sess.run(cls.W)[0][0]
+        back["b"] = cls.sess.run(cls.b)[0]
         return JsonResponse(data=back, code=status.HTTP_200_OK, desc='get house success') 
