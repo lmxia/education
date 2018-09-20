f876437861c3cbd0b66d9fa2503cfa5fde807783
lmxia <lmxia@alauda.io>
2018-09-20 16:22:23 +0800

    add background

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ curl -F 'file=@./tiger.jpeg' localhost:8000/image/process
 curl -X POST -H 'content-type:application/json' -d '{"points":[{"x":"1","y":"0.9"},{"x":"2.1","y":"1.9"},{"x":"2.9","y":"2.87"}]}' 18.216.109.187:8000/line/process
 git clone https://github.com/lmxia/education.git
 git clone https://github.com/tensorlayer/tensorlayer.git
-cd education && wget https://github.com/tensorlayer/pretrained-models/raw/master/models/vgg19.npy
+cd education && wget https://github.com/tensorlayer/pretrained-models/raw/master/models/vgg19.npy && wget https://github.com/tensorlayer/pretrained-models/raw/master/models/style_transfer_models_and_examples/pretrained_vgg19_encoder_model.npz \
+wget https://github.com/tensorlayer/pretrained-models/raw/master/models/style_transfer_models_and_examples/pretrained_vgg19_decoder_model.npz
 
 
 pip install django djangorestframework
